5fee13969faa2b69c74cc89d02d65da13a73c02b
minji <mini2ft@gmail.com>
2022-01-19 21:28:36 +0900

    롬복으로 전환

--- src/main/java/com/mini/springboot/web/HelloController.java
+++ src/main/java/com/mini/springboot/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.mini.springboot.web;
 
+import com.mini.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    // http://localhost:8080/hello/dto?name=kkk&amount=123
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
