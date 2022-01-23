cce24ff435a7954c5c54ffb4bbaca55cb4f0aa25
minji <mini2ft@gmail.com>
2022-01-23 14:11:56 +0900

    Posts 수정 Test

--- src/main/java/com/mini/springboot/web/PostsApiController.java
+++ src/main/java/com/mini/springboot/web/PostsApiController.java
@@ -5,10 +5,7 @@ import com.mini.springboot.web.dto.PostsResponseDto;
 import com.mini.springboot.web.dto.PostsSaveRequestDto;
 import com.mini.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -21,12 +18,12 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PostMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
     }
 
-    @PostMapping("/api/v1/posts/{id}")
+    @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
         return postsService.findById(id);
     }
--- src/test/java/com/mini/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/mini/springboot/web/PostsApiControllerTest.java
@@ -2,7 +2,9 @@ package com.mini.springboot.web;
 
 import com.mini.springboot.domain.posts.Posts;
 import com.mini.springboot.domain.posts.PostsRepository;
+import com.mini.springboot.web.dto.PostsResponseDto;
 import com.mini.springboot.web.dto.PostsSaveRequestDto;
+import com.mini.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -59,6 +63,43 @@ public class PostsApiControllerTest {
 
         assertThat(list.get(0).getTitle()).isEqualTo(title);
         assertThat(list.get(0).getContent()).isEqualTo(content);
+
+    }
+
+    @Test
+    public void Posts_수정() throws Exception {
+
+        Posts posts = postsRepository.save(
+                Posts.builder()
+                        .title("김민지")
+                        .content("김이요민지요")
+                        .author("kimminji")
+                        .build());
+
+        Long id = posts.getId();
+
+        String upd_title = "안녕하세요.";
+        String upd_content = "김민지입니다.";
+
+        PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+                .title(upd_title)
+                .content(upd_content)
+                .build();
+
+        String url = "http://localhost:" + port + "/api/v1/posts/" + id;
+
+        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+        ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+        List<Posts> list = postsRepository.findAll();
+
+        assertThat(list.get(0).getTitle()).isEqualTo(upd_title);
+        assertThat(list.get(0).getContent()).isEqualTo(upd_content);
+
 
     }
 }
