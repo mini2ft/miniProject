659780994a8a4ea1f44f1820b5120233a20536d7
minji <mini2ft@gmail.com>
2022-01-23 13:46:01 +0900

    Posts 업데이트

--- src/main/java/com/mini/springboot/web/PostsApiController.java
+++ src/main/java/com/mini/springboot/web/PostsApiController.java
@@ -2,7 +2,9 @@ package com.mini.springboot.web;
 
 import com.mini.springboot.service.posts.PostsService;
 import com.mini.springboot.web.dto.PostsSaveRequestDto;
+import com.mini.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,4 +19,10 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
 }
