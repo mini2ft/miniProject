494988b46e5404ad0e24c8406fe163fbcc6d43a6
minji <mini2ft@gmail.com>
2022-01-26 23:07:34 +0900

    게시글 삭제

--- src/main/java/com/mini/springboot/service/posts/PostsService.java
+++ src/main/java/com/mini/springboot/service/posts/PostsService.java
@@ -35,6 +35,16 @@ public class PostsService {
         return id;
     }
 
+    @Transactional
+    public void delete(Long id) {
+
+        Posts post = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+
+        postsRepository.delete(post);
+//        postsRepository.deleteById(id);
+    }
+
     public PostsResponseDto findById(Long id) {
 
         Posts posts = postsRepository.findById(id).orElseThrow(() ->
