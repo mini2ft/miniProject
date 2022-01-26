f1abcac40d574582dba07600d248bc7c6e2d7fbb
minji <mini2ft@gmail.com>
2022-01-26 22:57:43 +0900

    게시글 수정

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -4,8 +4,16 @@ var main = {
         $('#btn-save').on('click', function () {
             _this.save();
         });
-    }, save: function () {
-        var data = {title: $('#title').val(), author: $('#author').val(), content: $('#content').val()};
+        $('#btn-update').on('click', function () {
+            _this.update();
+        });
+    },
+    save: function () {
+        var data = {
+            title   : $('#title').val(),
+            author  : $('#author').val(),
+            content : $('#content').val()
+        };
         $.ajax({
             type: 'POST',
             url: '/api/v1/posts',
@@ -14,6 +22,25 @@ var main = {
             data: JSON.stringify(data)
         }).done(function () {
             alert('글이 등록되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        })
+    },
+    update: function () {
+        var data = {
+            title   : $('#title').val(),
+            content : $('#content').val()
+        };
+
+        $.ajax({
+            type: 'PUT',
+            url: '/api/v1/posts/' + $('#id').val(),
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(data)
+        }).done(function () {
+            alert('글이 수정되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
